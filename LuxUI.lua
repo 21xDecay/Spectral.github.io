9f77d544e9752a3334da42d4f12bc2e5d2845d72
21xDecay <128763846+21xDecay@users.noreply.github.com>
2024-09-23 16:18:04 -0400

    Update LuxUI.lua

--- LuxUI.lua
+++ LuxUI.lua
@@ -206,7 +206,7 @@ function Luxt1.CreateWindow(libName, logoId)
     wave.ImageTransparency = 0.85 -- Fully transparent
     wave.ScaleType = Enum.ScaleType.Slice
     wave.AnchorPoint = Vector2.new(0.5, 0.5) -- Set anchor point to center
-    wave.Position = UDim2.new(0.30, 0, 0.5, 0) -- Move slightly to the left
+    wave.Position = UDim2.new(0.35, 0, 0.5, 0) -- Move slightly to the left
 
 
     MainCorner_4.CornerRadius = UDim.new(0, 3)
